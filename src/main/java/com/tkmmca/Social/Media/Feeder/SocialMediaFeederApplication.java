e503f974328d0ddd4738d9ae488239be6ebe87cf
Razik <rasikmhd10@gmail.com>
2021-12-07 22:24:21 +0530

    INIT

---
+++ src/main/java/com/tkmmca/Social/Media/Feeder/SocialMediaFeederApplication.java
@@ -0,0 +1,13 @@
+package com.tkmmca.Social.Media.Feeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaFeederApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaFeederApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkmmca/Social/Media/Feeder/SocialMediaFeederApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkmmca.Social.Media.Feeder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaFeederApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
